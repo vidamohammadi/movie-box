7d92d7e968aa8d8c2f62a893801e2fa26340666b
m.vidamohammadi <m.vidamohammadi@gmail.com>
2025-04-07 17:24:11 +0330

    add: create navbar and add icons for navbar for desktop | header component

---
+++ components/header/desktop/Left.vue
@@ -0,0 +1,26 @@
+<script setup lang="ts">
+
+import {BellIcon, BookmarkIcon, MagnifyingGlassIcon, UserCircleIcon} from "@heroicons/vue/24/outline";
+</script>
+
+<template>
+  <div class="flex items-center space-x-2">
+    <button class="w-[48px] h-[48px] bg-[#1B1C1F] rounded-[4px] p-[12px] flex justify-center items-center">
+      <UserCircleIcon class="size-6 text-white" />
+    </button>
+    <button class="w-[48px] h-[48px] bg-[#1B1C1F] rounded-[4px] p-[12px] flex justify-center items-center">
+      <MagnifyingGlassIcon class="size-6 text-white" />
+    </button>
+    <button class="w-[48px] h-[48px] bg-[#1B1C1F] rounded-[4px] p-[12px] flex justify-center items-center">
+      <BookmarkIcon class="size-6 text-white" />
+    </button>
+    <button class="w-[48px] h-[48px] bg-[#1B1C1F] rounded-[4px] p-[12px] flex justify-center items-center">
+      <BellIcon class="size-6 text-white" />
+    </button>
+  </div>
+
+</template>
+
+<style scoped>
+
+</style>
---
+++ components/header/desktop/Navbar.vue
@@ -0,0 +1,15 @@
+<script setup lang="ts">
+
+</script>
+
+<template>
+  <header class="fixed top-0 left-0 z-50 w-full bg-[#242629] h-[64px]">
+    <div class="max-w-[1440px] mx-auto flex items-center justify-between px-[120px] py-[8px]">
+      <header-desktop-left />
+    </div>
+  </header>
+</template>
+
+<style scoped>
+
+</style>
